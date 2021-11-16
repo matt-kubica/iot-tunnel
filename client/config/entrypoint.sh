bf2917fe531b3c8d24c4e782fa0ac728ff497701
qbic22562 <mateusz.kubica99@gmail.com>
2021-11-16 23:08:54 +0100

    IT-1: Changes client's base configuration, adds client-configurer service, moves $OVPN_SERVER_IP to .env.dev file, delegates ansible related files to separate directory

--- client/config/entrypoint.sh
+++ client/config/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-IP=$1
-
 cfssl gencert -remote=ca:8888 -profile=client client-csr.json | cfssljson -bare client
 
 cp /etc/common/ca.crt .
@@ -10,7 +8,7 @@ mv client.pem client.crt
 mv client-key.pem client.key
 
 cat base.conf \
-	<(echo -e "\nremote $IP 443\n") \
+	<(echo -e "\nremote $OVPN_SERVER_IP 443\n") \
     <(echo -e "<ca>") \
     ./ca.crt \
     <(echo -e "</ca>\n<cert>") \
@@ -19,4 +17,4 @@ cat base.conf \
     ./client.key \
     <(echo -e "</key>\n<tls-crypt>") \
     ./ta.key \
-    <(echo -e "</tls-crypt>")
+    <(echo -e "</tls-crypt>") > /etc/common/client.conf
